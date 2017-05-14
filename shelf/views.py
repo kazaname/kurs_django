5194654ace7be3bf2e3d49311b829c94ced168ed
kazaname <suriiw@gmail.com>
2017-05-14 12:19:29 +0200

    Druga lekcja kursu 'Django od podstaw'. Twprzenie widoków, praca z ulr`am

--- shelf/views.py
+++ shelf/views.py
@@ -2,5 +2,14 @@
 from __future__ import  unicode_literals, absolute_import
 
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Author
 
-# Create your views here.
+
+class AuthorListView(ListView):
+    model = Author
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+
